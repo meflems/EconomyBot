9fb4f179eac80f32828caf6b6c0c091dd9f35b7b
meflems <89581469+meflems@users.noreply.github.com>
2022-04-04 22:42:11 +0700

    update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on("message", async message => {
                 var name = message.author.username;
                 server.channels.create(name, "text");
                 const ChID = Discord.channels.cache.find(channel => channel.name === name);
-                client.message.channel.send(ChID);
+                client.message.channel.send(ChID).catch(err) { console.log(err) };
                 break
         }
     })
